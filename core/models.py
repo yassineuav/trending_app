23b2be4625f861e3d88232f6fc605dd3911fcf87
Yassine <yassineuav@gmail.com>
2022-08-07 17:41:23 -0600

    Initial commit.

--- core/models.py
+++ core/models.py
@@ -11,21 +11,3 @@ class User(AbstractUser):
     zipCode = models.PositiveIntegerField(default=0)
     imageUrl = models.ImageField(upload_to='profile', default='')
 
-
-class InterestedIn(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    popular = models.IntegerField(default=0)
-    usersIn = models.IntegerField(default=0)
-    suggestedTimes = models.IntegerField(default=0)
-    display = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    imageUrl = models.ImageField(upload_to='interested/images', default='')
-    iconUrl = models.ImageField(upload_to='interested/icons', default='')
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    description = models.TextField()
-    imageUrl = models.ImageField(upload_to='post/images', default='')
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class InterestedIn(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    popular = models.IntegerField(default=0)
+    usersIn = models.IntegerField(default=0)
+    suggestedTimes = models.IntegerField(default=0)
+    display = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    imageUrl = models.ImageField(upload_to='interested/images', default='')
+    iconUrl = models.ImageField(upload_to='interested/icons', default='')
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    description = models.TextField()
+    imageUrl = models.ImageField(upload_to='post/images', default='')
